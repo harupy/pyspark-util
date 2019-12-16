fe2a8964c5b82ccd8b30ddc57fd910b0e6ab5dd4
Harutaka Kawamura <hkawamura0130@gmail.com>
2019-12-17 02:14:31 +0900

    Use index as master_doc (#15)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,9 @@ templates_path = ['_templates']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+# The master toctree document.
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
